89a59899897fd8cfe1708420962cf4539478bd8f
vijay chauhan <vijaychauhan0056@gmail.com>
2022-09-04 01:00:02 +0530

    fix

--- Vjtoast/src/main/java/com/vjchauhan/vjtoast/Toaster.kt
+++ Vjtoast/src/main/java/com/vjchauhan/vjtoast/Toaster.kt
@@ -4,7 +4,7 @@ import android.content.Context
 import android.widget.Toast
 
 
-class Toaster {
+object  Toaster {
     fun s(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
--- app/src/main/java/com/vjchauhan/mylibrary/MainActivity.kt
+++ app/src/main/java/com/vjchauhan/mylibrary/MainActivity.kt
@@ -2,10 +2,12 @@ package com.vjchauhan.mylibrary
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vjchauhan.vjtoast.Toaster
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        Toaster.s(this,"")
     }
 }
