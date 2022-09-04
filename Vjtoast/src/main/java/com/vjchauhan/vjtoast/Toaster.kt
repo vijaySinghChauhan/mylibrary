26654f0805d94aadafab15511d6ff597af981b36
vijay chauhan <vijaychauhan0056@gmail.com>
2022-09-04 09:39:34 +0530

    toast fix

--- Vjtoast/src/main/java/com/vjchauhan/vjtoast/Toaster.kt
+++ Vjtoast/src/main/java/com/vjchauhan/vjtoast/Toaster.kt
@@ -5,7 +5,7 @@ import android.widget.Toast
 
 
 object  Toaster {
-    fun call(c: Context?, message: String?) {
+    fun pop(c: Context?, message: String?) {
         Toast.makeText(c, message, Toast.LENGTH_SHORT).show()
     }
 }
